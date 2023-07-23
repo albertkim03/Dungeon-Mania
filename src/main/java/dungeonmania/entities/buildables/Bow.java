86bae3480f27684421044fd99c5f77059b608da2
christopher <z5373462@ad.unsw.edu.au>
2023-07-23 16:19:57 +1000

    Modified Bow.java, shield.java buildable.java, changed inheritance structure for methods getDurability, applybuff, and use

--- src/main/java/dungeonmania/entities/buildables/Bow.java
+++ src/main/java/dungeonmania/entities/buildables/Bow.java
@@ -1,31 +1,33 @@
 package dungeonmania.entities.buildables;
 
-import dungeonmania.Game;
-import dungeonmania.battles.BattleStatistics;
+//import dungeonmania.Game;
+//import dungeonmania.battles.BattleStatistics;
 
 public class Bow extends Buildable {
-    private int durability;
+    // private int durability;
 
     public Bow(int durability) {
         super(null);
-        this.durability = durability;
+        this.setDurability(durability);
+        this.setAttackMagnifier(2);
+        this.setDamageReducer(1);
     }
 
-    @Override
-    public void use(Game game) {
-        durability--;
-        if (durability <= 0) {
-            game.getPlayer().remove(this);
-        }
-    }
+    // @Override
+    // public void use(Game game) {
+    //     durability--;
+    //     if (durability <= 0) {
+    //         game.getPlayer().remove(this);
+    //     }
+    // }
 
-    @Override
-    public BattleStatistics applyBuff(BattleStatistics origin) {
-        return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, 0, 2, 1));
-    }
+    // @Override
+    // public BattleStatistics applyBuff(BattleStatistics origin) {
+    //     return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, 0, 2, 1));
+    // }
 
-    @Override
-    public int getDurability() {
-        return durability;
-    }
+    // @Override
+    // public int getDurability() {
+    //     return durability;
+    // }
 }
