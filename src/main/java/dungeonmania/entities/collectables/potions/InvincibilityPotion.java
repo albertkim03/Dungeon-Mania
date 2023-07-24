e3732f97e2928a8873ef7c1f39dbb830cb2c3909
christopher <z5373462@ad.unsw.edu.au>
2023-07-24 23:07:10 +1000

    Task1f, changed inheritance structure of potion.java

--- src/main/java/dungeonmania/entities/collectables/potions/InvincibilityPotion.java
+++ src/main/java/dungeonmania/entities/collectables/potions/InvincibilityPotion.java
@@ -1,6 +1,6 @@
 package dungeonmania.entities.collectables.potions;
 
-import dungeonmania.battles.BattleStatistics;
+// import dungeonmania.battles.BattleStatistics;
 import dungeonmania.util.Position;
 
 public class InvincibilityPotion extends Potion {
@@ -8,10 +8,14 @@ public class InvincibilityPotion extends Potion {
 
     public InvincibilityPotion(Position position, int duration) {
         super(position, duration);
+        this.setAttackMagnifier(1);
+        this.setDamageReducer(1);
+        this.setIsInvincible(true);
+        this.setIsEnabled(true);
     }
 
-    @Override
-    public BattleStatistics applyBuff(BattleStatistics origin) {
-        return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, 0, 1, 1, true, true));
-    }
+    // @Override
+    // public BattleStatistics applyBuff(BattleStatistics origin) {
+    //     return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, 0, 1, 1, true, true));
+    // }
 }
--- src/main/java/dungeonmania/entities/collectables/potions/Potion.java
+++ src/main/java/dungeonmania/entities/collectables/potions/Potion.java
@@ -11,6 +11,26 @@ import dungeonmania.util.Position;
 
 public abstract class Potion extends Entity implements InventoryItem, BattleItem {
     private int duration;
+    private int attackMagnifier;
+    private int damageReducer;
+    private boolean isInvincible;
+    private boolean isEnabled;
+
+    public void setAttackMagnifier(int attackMagnifier) {
+        this.attackMagnifier = attackMagnifier;
+    }
+
+    public void setDamageReducer(int damageReducer) {
+        this.damageReducer = damageReducer;
+    }
+
+    public void setIsInvincible(boolean isInvincible) {
+        this.isInvincible = isInvincible;
+    }
+
+    public void setIsEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
 
     public Potion(Position position, int duration) {
         super(position);
@@ -31,15 +51,15 @@ public abstract class Potion extends Entity implements InventoryItem, BattleItem
         }
     }
 
-    @Override
-    public void onDestroy(GameMap gameMap) {
-        return;
-    }
+    // @Override
+    // public void onDestroy(GameMap gameMap) {
+    //     return;
+    // }
 
-    @Override
-    public void onMovedAway(GameMap map, Entity entity) {
-        return;
-    }
+    // @Override
+    // public void onMovedAway(GameMap map, Entity entity) {
+    //     return;
+    // }
 
     @Override
     public void use(Game game) {
@@ -52,7 +72,9 @@ public abstract class Potion extends Entity implements InventoryItem, BattleItem
 
     @Override
     public BattleStatistics applyBuff(BattleStatistics origin) {
-        return origin;
+        return BattleStatistics.applyBuff(origin,
+         new BattleStatistics(0, 0, 0,
+          attackMagnifier, damageReducer, isInvincible, isEnabled));
     }
 
     @Override
